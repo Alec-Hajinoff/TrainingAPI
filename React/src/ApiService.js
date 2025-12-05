1fcb33b305dd62fc4180703d3483b00b9835f33d
Alec Hajinoff <hajinoff@gmail.com>
2025-12-05 16:49:11 +0000

    5.12.25, 16.49

--- React/src/ApiService.js
+++ React/src/ApiService.js
@@ -68,7 +68,7 @@ export const createActionFunction = async (formData) => {
 export const logoutUser = async () => {
   try {
     const response = await fetch(
-      "http://localhost:8001/Sustainability_Log_Development/logout_component.php",
+      "http://localhost:8001/TrainingAPI/logout_component.php",
       {
         method: "POST",
         credentials: "include",
@@ -81,108 +81,5 @@ export const logoutUser = async () => {
   } catch (error) {
     console.error("Error during logout:", error);
     throw new Error("An error occurred during logout.");
-  }
-};
-
-// userDashboard() fetches data from the database to populate the company-user dashboard in CreateAction.js.
-
-export const userDashboard = async () => {
-  try {
-    const response = await fetch(
-      "http://localhost:8001/Sustainability_Log_Development/user_dashboard.php",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-      }
-    );
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error("Failed to fetch dashboard data");
-  }
-};
-
-// searchCompanyNames() fetches company name suggestions for @mentions
-
-export const searchCompanyNames = async (searchTerm) => {
-  try {
-    const response = await fetch(
-      "http://localhost:8001/Sustainability_Log_Development/company_name_search.php",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({ searchTerm }),
-      }
-    );
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error("Failed to search for companies");
-  }
-};
-
-// fetchTimeline() retrieves public sustainability actions for a company by slug.
-
-export const fetchTimeline = async (slug) => {
-  try {
-    const response = await fetch(
-      `http://localhost:8001/Sustainability_Log_Development/get_timeline.php?slug=${slug}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-      }
-    );
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error("Failed to load timeline");
-  }
-};
-
-// fetchCompanyMap() fetches URLs from the database to make mentions clickable.
-
-export const fetchCompanyMap = async () => {
-  try {
-    const response = await fetch(
-      "http://localhost:8001/Sustainability_Log_Development/get_company_urls.php",
-      {
-        method: "GET",
-        credentials: "include",
-      }
-    );
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    throw new Error("Failed to fetch company URLs");
-  }
-};
-
-// fetchUserTimelineUrlQr() fetches company URL which is displayed just above the timeline.
-
-export const fetchUserTimelineUrlQr = async () => {
-  try {
-    const response = await fetch(
-      "http://localhost:8001/Sustainability_Log_Development/get_user_company_url_qr.php",
-      {
-        method: "GET",
-        credentials: "include",
-      }
-    );
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    throw new Error("Failed to fetch user timeline URL and QR code");
   }
 };
