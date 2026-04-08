96d9103feb37ed5acdd2bc5787202aa89733ed8f
Alec Hajinoff <hajinoff@gmail.com>
2026-04-08 15:21:15 +0100

    8/4/26, 15:21

--- delete_workshop_requested.php
+++ delete_workshop_requested.php
@@ -1,19 +1,25 @@
 <?php
 require_once 'session_config.php';
 
-$allowed_origins = ['http://localhost:3000'];
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+$allowed_origins = [
+    'http://localhost:3000',
+    'https://trainingapi.com',
+    'https://www.trainingapi.com'
+];
 
-if (in_array($origin, $allowed_origins)) {
+$origin = $_SERVER['HTTP_ORIGIN'] ?? null;
+
+if ($origin !== null && in_array($origin, $allowed_origins)) {
     header("Access-Control-Allow-Origin: $origin");
+} elseif ($origin === null) {
 } else {
     header('HTTP/1.1 403 Forbidden');
     exit;
 }
 
-header('Access-Control-Allow-Methods: POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
