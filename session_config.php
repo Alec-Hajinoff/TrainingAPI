4c0b753ae66fa108dea8fd15dbe20aeec361cd5d
Alec Hajinoff <hajinoff@gmail.com>
2026-04-12 15:06:41 +0100

    12/4/26, 15:06

--- session_config.php
+++ session_config.php
@@ -1,7 +1,7 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
     session_set_cookie_params([
-        'lifetime' => 3600,
+        'lifetime' => 86400,
         'path' => '/',
         'domain' => 'localhost',    // Use 'climatebind.com' in production
         'secure' => false,          // Change to true when using HTTPS
@@ -13,7 +13,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$timeout = 1800;
+$timeout = 86400;
 
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $timeout)) {
     session_unset();
